9d3ac1de32541bd52dd596e02aeeb1f59a15e37f
Emil John <emiljohn1129@gmail.com>
2023-02-10 15:56:31 +0800

    fixed database connection hehe

--- application/application.php
+++ application/application.php
@@ -22,7 +22,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Jaydee C. Ballaho">
+    <meta name="author" content="">
     <title>CCS Programs</title>
     <link rel="icon" href="../img/ccslogo.png" type="image/icon type">
     
