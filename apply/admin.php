faad7cf3352793e6d0d6df070337e173bea5ef54
Emil John <emiljohn1129@gmail.com>
2023-02-25 12:46:07 +0800

    Update admin.php

--- apply/admin.php
+++ apply/admin.php
@@ -1,3 +1,13 @@
+<?php
+
+    session_start();
+
+    if (!isset($_SESSION['logged-in'])){
+        header('location: ../login/login.php');
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -40,7 +50,16 @@ $result = mysqli_query($conn, $sql);
 
 echo '<table>';
 echo '<thead>';
-echo '<tr><th>ID</th><th>Name</th><th>Subjects</th><th>Units</th><th>Grades</th><th>General Point Average</th><th>Action</th></tr>';
+echo '<tr><th>ID</th><th>Name</th><th>Subjects</th><th>Units</th><th>Grades</th><th>General Point Average</th>
+
+            <?php
+                if($_SESSION["user_type"] == "admin"){ 
+            ?>
+                <th class="action">Action</th>
+            <?php
+                    }
+            ?>
+    </tr>';
 echo '</thead>';
 echo '<tbody>';
 while ($row = mysqli_fetch_assoc($result)) {
@@ -51,7 +70,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     echo '<td width="5%">' . htmlspecialchars($row['units']) . '</td>';
     echo '<td >' . htmlspecialchars($row['grade']) . '</td>';
     echo '<td>' . htmlspecialchars($row['average'  ]) .'</td>';
-
+    
     echo '<td width="30%">';
     echo '<form method="post">';
     echo '<input type="hidden" name="id" value="' . htmlspecialchars($row['id']) . '">';
