dd0e7fd3d248a1a071b9d6e68ca68c64dafac652
Emil John <emiljohn1129@gmail.com>
2023-03-28 01:30:01 +0800

    Update listers.php

--- listers/listers.php
+++ listers/listers.php
@@ -223,6 +223,7 @@
                         <tr>
                             <th>Rank</th>
                             <th>Name</th>
+                            <th>Email</th>
                             <th>GPA</th>
                             <th>Department</th>
                             <th>Year Level</th>
@@ -250,6 +251,7 @@
                                 <!-- always use echo to output PHP values -->
                                 <td><?php echo $i ?></td>
                                 <td><?php echo $value['fullname'] ?>
+                                <td><?php echo $value['email'] ?>
                                 <td><?php echo $value['gpa'] ?></td>
                                 <td><?php echo $value['department'] ?></td>
                                 <td><?php echo $value['yearlevel'] ?></td>
